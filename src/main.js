a17f7aab955e17ce2d2cf22fb91f3d9a87e277c2
cristinarodriguezm <nothing.matters@hotmail.com>
2018-07-28 21:58:39 -0500

    login email, facebook y gmail

--- src/main.js
+++ src/main.js
@@ -3,7 +3,22 @@ const signIn = document.getElementById("signinbtn");
 const register = document.getElementById("register");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
+const btnGoogle = document.getElementById("btnGoogle");
+const btnFacebook = document.getElementById("btnFacebook");
 
+window.onload = () => {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+         login.classList.remove("hidden");
+         logout.classList.add("hidden");   
+          console.log("Usuario logueado")
+        } else {
+          console.log("No esta logueado")
+          login.classList.add("hidden");
+          logout.classList.remove("hidden");
+        }
+      });
+}
 
 register.addEventListener("click", () => {
 firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
@@ -23,5 +38,50 @@ firebase.auth().signInWithEmailAndPassword(email.value, password.value)
 .catch(function(error) {
     console.log(error.code, error.message);
   });
+})
+
+logOut.addEventListener("click", () => {
+    firebase.auth().signOut()
+        .then(function() {
+        console.log("Cerro Sesion");
+        login.classList.remove("hidden");
+        logout.classList.add("hidden");
+
+    }). catch(function(error) {
+        console.log("Error al cerrar sesion")
+    });
+})
+
+btnGoogle.addEventListener("click", () =>{
+    let provider= new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then(function(result) {
+        console.log("Sesion con Google");
+        let user = result.user;
+        writeUserData(user.uid, user.displayName, user.email, user.photoURL)
+      }).catch(function(error) {
+       
+        console.log(error.code);
+        console.log(error.message);
+        console.log(error.email);
+        console.log(error.credential);
+      
+      });
+})
+
+btnFacebook.addEventListener("click", () => {
+    let provider = new firebase.auth.FacebookAuthProvider();
+    provider.setCustomParameters({
+        'display' : 'popup'
+    });
+    firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
+        console.log("Logueado con Facebook")
+      }).catch(function(error) {
+        console.log(error.code);
+        console.log(error.message);
+        console.log(error.email);
+        console.log(error.credential);
+       });
 })
 
