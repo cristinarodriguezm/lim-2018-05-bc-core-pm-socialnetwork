079989d8fe2765ca1b521a72fc21f5674a95756c
cristinarodriguezm <nothing.matters@hotmail.com>
2018-08-02 23:28:41 -0500

    pruebas de post

---
+++ src/data.js
@@ -0,0 +1,49 @@
+window.onload = () => {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            login.classList.add("hidden");
+            logout.classList.remove("hidden");
+            wall.classList.remove("hidden");
+            posts.classList.remove("hiden");
+            username.innerHTML = `Bienvenida ${user.displayName}`;
+            console.log("Usuario logueado");
+        } else {
+            console.log("No esta logueado")
+            login.classList.remove("hidden");
+            logout.classList.add("hidden");
+            wall.classList.add("hidden");
+            posts.classList.add("hiden");
+        }
+    });
+}
+
+
+window.writeUserData = (userId, name, email, imageUrl) => {
+    firebase.database().ref('users/' + userId).set({
+        username: name,
+        email: email,
+        profile_picture: imageUrl,
+        displayName: name
+    });
+}
+
+window.writeNewPost = (uid, body) => {
+    // A post entry.
+    var postData = {
+        uid: uid,
+        body: body,
+    };
+
+    // Get a key for a new Post.
+    var newPostKey = firebase.database().ref().child('posts').push().key;
+
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    var updates = {};
+    updates['/posts/' + newPostKey] = postData;
+    updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+    firebase.database().ref().update(updates);
+    return newPostKey
+}
+
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-const logOut = document.getElementById("btnLogout");
-const signIn = document.getElementById("signinbtn");
+const login = document.getElementById("login");
+const logout = document.getElementById("logout")
+const btnLogOut = document.getElementById("btnLogout");
+const btnSignIn = document.getElementById("signinbtn");
 const register = document.getElementById("register");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
@@ -8,67 +10,59 @@ const btnFacebook = document.getElementById("btnFacebook");
 const wall = document.getElementById("wall");
 const btnPost = document.getElementById("btnPost");
 const post = document.getElementById("post");
-const posts = document.getElementById("posts")
+const posts = document.getElementById("posts");
+const username = document.getElementById("user-name");
 
+btnPost.addEventListener('click', () => {
+    var userId = firebase.auth().currentUser.uid;
+    const newPost = writeNewPost(userId, post.value);
+  
 
-window.onload = () => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            login.classList.remove("hidden");
-            logout.classList.add("hidden");
-            wall.classList.remove("hidden");
-            posts.classList.remove("hiden");
-            console.log("Usuario logueado");
-        } else {
-            console.log("No esta logueado")
-            login.classList.add("hidden");
-            logout.classList.remove("hidden");
-            wall.classList.add("hidden");
-            posts.classList.add("hiden");
-        }
-    });
-}
-
-function writeUserData(userId, name, email, imageUrl) {
-    firebase.database().ref('users/' + userId).set({
-        username: name,
-        email: email,
-        profile_picture: imageUrl
-    });
-}
-
-function writeNewPost(uid, body) {
-    // A post entry.
-    var postData = {
-        uid: uid,
-        body: body,
-    };
-
-    // Get a key for a new Post.
-    var newPostKey = firebase.database().ref().child('posts').push().key;
-
-    // Write the new post's data simultaneously in the posts list and the user's post list.
-    var updates = {};
-    updates['/posts/' + newPostKey] = postData;
-    updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-
-    firebase.database().ref().update(updates);
-    return newPostKey
-}
-btnPost.addEventListener("click", ()=> {
-    let userId = firebase.auth().currentUser.uid;
-    let newPost = writeNewPost(userId, post.value);
-
-    let btnUpdate = document.createElement("input");
+    var btnUpdate = document.createElement("input");
     btnUpdate.setAttribute("value", "Update");
-    btnUpdate.setAttribute("type", "button")
-    let btnDelete = document.createElement("input");
+    btnUpdate.setAttribute("type", "button");
+    var btnDelete = document.createElement("input");
     btnDelete.setAttribute("value", "Delete");
     btnDelete.setAttribute("type", "button");
-    let contPost = document.createElement("div");
-    let textPost = document.createElement("textarea");
+    var contPost = document.createElement('div');
+    var textPost = document.createElement('textarea')
+    textPost.setAttribute("id", newPost);
+
     textPost.innerHTML = post.value;
 
+    btnDelete.addEventListener('click', () => {
+
+        firebase.database().ref().child('/user-posts/' + userId + '/' + newPost).remove();
+        firebase.database().ref().child('posts/' + newPost).remove();
+
+        while (posts.firstChild) posts.removeChild(posts.firstChild);
+
+        alert('The user is deleted successfully!');
+        reload_page();
+
+    });
+
+    btnUpdate.addEventListener('click', () => {
+        const newUpdate = document.getElementById(newPost);
+        const nuevoPost = {
+            body: newUpdate.value,
+        };
+
+        var updatesUser = {};
+        var updatesPost = {};
+
+        updatesUser['/user-posts/' + userId + '/' + newPost] = nuevoPost;
+        updatesPost['/posts/' + newPost] = nuevoPost;
+
+        firebase.database().ref().update(updatesUser);
+        firebase.database().ref().update(updatesPost);
+
+    });
+
+    contPost.appendChild(textPost);
+    contPost.appendChild(btnUpdate);
+    contPost.appendChild(btnDelete);
+    posts.appendChild(contPost);
 })
 
 register.addEventListener("click", () => {
@@ -81,7 +75,7 @@ register.addEventListener("click", () => {
         });
 })
 
-signIn.addEventListener("click", () => {
+btnSignIn.addEventListener("click", () => {
     firebase.auth().signInWithEmailAndPassword(email.value, password.value)
         .then(function () {
             console.log("Inicia sesion");
@@ -93,7 +87,7 @@ signIn.addEventListener("click", () => {
         });
 })
 
-logOut.addEventListener("click", () => {
+btnLogOut.addEventListener("click", () => {
     firebase.auth().signOut()
         .then(function () {
             console.log("Cerro Sesion");
